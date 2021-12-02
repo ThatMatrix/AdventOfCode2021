2f4059b307cc1f377ea8abfbea8577f284b6ff0c
ThatMatrix <noe.urso1212@gmail.com>
2021-12-02 20:44:36 +0100

    finished day2 + preparation for day3

--- Day2/Main.cs
+++ Day2/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Day2
 {
@@ -6,7 +7,51 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = File.ReadAllLines("../../../input");
+
+            int result = FollowPath(lines);
+            if (result ==  -1)
+            {
+                Console.Error.WriteLine("One String had not the appropriate format");
+                return;
+            }
+            
+            Console.WriteLine("The Depth multiplied by the horizontal position is: {0}", result);
+        }
+
+        static int FollowPath(string[] lines)
+        {
+            int aim = 0;
+            int horizontal = 0;
+            int depth = 0;
+            
+            foreach (var line in lines)
+            {
+                string[] separated = line.Split(' ');
+                if (separated.Length != 2)
+                {
+                    return -1;
+                }
+                
+                string direction = separated[0];
+                switch (direction[0])
+                {
+                    case 'f':
+                        horizontal += int.Parse(separated[1]);
+                        depth += aim * int.Parse(separated[1]);
+                        break;
+                    case 'd':
+                        aim += int.Parse(separated[1]);
+                        break;
+                    case 'u':
+                        aim -= int.Parse(separated[1]);
+                        break;
+                    default:
+                        return -1;
+                }
+            }
+
+            return horizontal * depth;
         }
     }
 }
---
+++ Day3/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../input");
+            return;
+        }
+    }
+}
