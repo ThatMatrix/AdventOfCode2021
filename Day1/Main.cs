8a7684563abaf284a9e338800bf77326a604608f
ThatMatrix <noe.urso1212@gmail.com>
2021-12-02 20:26:47 +0100

    Finished Day1 for real

--- Day1/Main.cs
+++ Day1/Main.cs
@@ -9,6 +9,13 @@ namespace Day1
         {
             string[] lines = File.ReadAllLines("../../../input");
             
+            int count = NormalMesurement(lines);
+            int count2 = SlidingWindow(lines);
+            Console.WriteLine("Decreased {0} times", count2);
+        }
+
+        static int NormalMesurement(string[] lines)
+        {
             int length = lines.Length;
             int[] parsed = new int[length];
             int count = 0;
@@ -22,7 +29,31 @@ namespace Day1
                 }
             }
 
-            Console.WriteLine("Decreased {0} times", count);
+            return count;
+        }
+
+        static int SlidingWindow(string[] lines)
+        {
+            int length = lines.Length;
+            int[] parsed = new int[length];
+            int[] sliding = new int[length - 2];
+            int count = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                parsed[i] = int.Parse(lines[i]);
+                if (i > 1)
+                {
+                    sliding[i - 2] = parsed[i] + parsed[i - 1] + parsed[i - 2];
+                }
+
+                if (i > 2 && sliding[i - 2] > sliding[i - 3])
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
