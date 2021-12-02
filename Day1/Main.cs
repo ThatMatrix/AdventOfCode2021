b53b331439013ba998c443cfc3da4db86d5eb045
ThatMatrix <noe.urso1212@gmail.com>
2021-12-02 19:13:57 +0100

    finished Day1

---
+++ Day1/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../input");
+            
+            int length = lines.Length;
+            int[] parsed = new int[length];
+            int count = 0;
+            
+            for (int i = 0; i < length; i++)
+            {
+                parsed[i] = int.Parse(lines[i]);
+                if (i > 0 && parsed[i - 1] < parsed[i])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Decreased {0} times", count);
+        }
+    }
+}
