52f81f132fd2a3a926a14fe269807b0259272bd3
ThatMatrix <noe.urso1212@gmail.com>
2021-12-03 13:53:31 +0100

    finished Day3 part1

--- Day3/Main.cs
+++ Day3/Main.cs
@@ -7,8 +7,42 @@ namespace Day3
     {
         static void Main(string[] args)
         {
+            Part1();
+        }
+
+        static void Part1()
+        {
             string[] lines = File.ReadAllLines("../../../input");
-            return;
+            int gamma = 0;
+            int epsilon = 0;
+            
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                int countOnes = 0;
+
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    string line = lines[j];
+                    if (line[i] == '1')
+                    {
+                        countOnes += 1;
+                    }
+                }
+
+                if (countOnes > lines.Length/2)
+                {
+                    gamma = gamma * 2 + 1;
+                    
+                    epsilon = epsilon * 2;
+                }
+                else
+                {
+                    gamma = gamma * 2;
+                    epsilon = epsilon * 2 + 1;
+                }
+            }
+
+            Console.WriteLine("Part1: The value of epsilon multiplied by gamma is: {0}", gamma * epsilon);
         }
     }
 }
