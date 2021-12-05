9eba98ebbb8a6534cd156b2006e7e35b7781833f
ThatMatrix <noe.urso1212@gmail.com>
2021-12-05 15:21:10 +0100

    Day5 archi

---
+++ Day5/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../example");
+        }
+    }
+}
